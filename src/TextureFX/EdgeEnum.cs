d6c1922ee477a1c99606c52bde7be6387a605809
cloneproduction <clone@cloneproduction.net>
2023-05-04 18:40:08 +0800

    Added Edge Shader and Enum

---
+++ src/TextureFX/EdgeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum Edge
+    {
+        [Description("Sobel")]
+        Sobel = 0,
+        [Description("MotorSaw")]
+        MotorSaw = 1,
+        [Description("Aiekick")]
+        Aiekick = 2,
+        [Description("Charcoal")]
+        Charcoal = 3,
+    }
+}
