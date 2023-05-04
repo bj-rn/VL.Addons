b65a6708ca3bf458a6d22dd590ac2696d65e0bd4
cloneproduction <clone@cloneproduction.net>
2023-05-04 11:54:08 +0800

    Added some new TFX and ShaderFX
    New ColorMap with ColorSpace selection, ValueMap with Channel selection.
    New ColorMatrix, Invert in both TFX and ShaderFX

---
+++ src/TextureFX/ColorChannelEnum.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum ColorChannel
+    {
+        [Description("Red")]
+        Red = 0,
+        [Description("Green")]
+        Green = 1,
+        [Description("Blue")]
+        Blue = 2,
+        [Description("Alpha")]
+        Alpha = 3,
+        [Description("Value")]
+        Value = 4,
+        [Description("Lightness")]
+        Lightness = 5,
+        [Description("Saturation")]
+        Saturation = 6,
+        [Description("Hue")]
+        Hue = 7,
+        [Description("Cyan")]
+        Cyan = 8,
+        [Description("Magenta")]
+        Magenta = 9,
+        [Description("Yellow")]
+        Yellow = 10,
+        [Description("Black")]
+        Black = 11,
+    }
+}
---
+++ src/TextureFX/ColorSpaceEnum.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum ColorSpace
+    {
+        [Description("RGBA")]
+        RGBA = 0,
+        [Description("HSVA")]
+        HSVA = 1,
+        [Description("HSLA")]
+        HSLA = 2,
+        [Description("CMYK")]
+        CMYK = 3,
+        [Description("OKLAB")]
+        OKLAB = 4,
+    }
+}
