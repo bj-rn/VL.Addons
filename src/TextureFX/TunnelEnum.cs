37ba973c23306db86105a85a68101d5067cbde76
cloneproduction <clone@cloneproduction.net>
2023-05-04 23:25:24 +0800

    Added Tunnels with Help Patch

---
+++ src/TextureFX/TunnelEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum TunnelType
+    {
+        [Description("Square")]
+        Square = 0,
+        [Description("Cylinder")]
+        Cylinder = 1,
+        [Description("Fly")]
+        Fly = 2,
+    }
+}
