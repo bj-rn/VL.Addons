350b89a9d79ae295fe2d9e93833235acbb896ed2
cloneproduction <clone@cloneproduction.net>
2023-05-05 11:34:08 +0800

    Added ColorWaveShaper with Help Patch

---
+++ src/TextureFX/ColorWaveEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum ColorWave
+    {
+        [Description("Linear")]
+        Linear = 0,
+        [Description("Inverse")]
+        Inverse = 1,
+        [Description("Triangle")]
+        Triangle = 2,
+        [Description("Sin")]
+        Sin = 3,
+        [Description("Rectangle")]
+        Rectangle = 4,
+        [Description("Frac")]
+        Frac = 5
+    }
+}
