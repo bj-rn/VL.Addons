99402a7b4def0a32aef256222a65b2243850271a
cloneproduction <clone@cloneproduction.net>
2023-05-05 21:54:52 +0800

    Added ColorGrading with Enum and Help Patch

---
+++ src/TextureFX/AdjustementWeight.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum AdjustementWeight
+    {
+        [Description("Full")]
+        Full = 0,
+        [Description("Shadows")]
+        Shadows = 1,
+        [Description("Midtones")]
+        Midtones = 2,
+        [Description("Highlights")]
+        Highlights = 3,
+
+        [Description("Red")]
+        Red = 4,
+        [Description("Green")]
+        Green = 5,
+        [Description("Blue")]
+        Blue = 6,
+
+        [Description("Value")]
+        Value = 7,
+        [Description("Lightness")]
+        Lightness = 8,
+        [Description("Saturation")]
+        Saturation = 9,
+        [Description("Hue")]
+        Hue = 10,
+
+        [Description("Cyan")]
+        Cyan = 11,
+        [Description("Magenta")]
+        Magenta = 12,
+        [Description("Yellow")]
+        Yellow = 13,
+        [Description("Black")]
+        Black = 14,
+
+        [Description("Alpha")]
+        Alpha = 15,
+        [Description("Long")]
+        Long = 16,
+        [Description("Medium")]
+        Medium = 17,
+        [Description("Short")]
+        Short = 18,
+    }
+}
---
+++ src/TextureFX/ColorGradingFunction.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum ColorGradingFunction
+    {
+        [Description("Brightness")]
+        Brightness = 0,
+        [Description("Contrast")]
+        Contrast = 1,
+        [Description("Saturation")]
+        Saturation = 2,
+        [Description("Hue")]
+        Hue = 3,
+        [Description("Gamma")]
+        Gamma = 4,
+        [Description("Exposure")]
+        Exposure = 5,
+        [Description("Gain")]
+        Gain = 6,
+        [Description("Lift")]
+        Lift = 7,
+    }
+}
