8e0423697ad0deccd52e870244091b78ce9f7bc1
cloneproduction <clone@cloneproduction.net>
2023-05-05 22:22:45 +0800

    Added Glitch with Help Patch

---
+++ src/TextureFX/GlitchEnum.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum Glitch
+    {
+        [Description("Displace")]
+        Displace = 0,
+        [Description("DispColorLine")]
+        DispColorLine = 1,
+        [Description("RGBLinesHorizontal")]
+        RGBLinesHorizontal = 2,
+        [Description("RGBLinesVertical")]
+        RGBLinesVertical = 3,
+        [Description("Luma")]
+        Luma = 4,
+    }
+}
---
+++ src/TextureFX/Orientation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum EffectOrientation
+    {
+        [Description("Horizontal")]
+        Horizontal = 0,
+        [Description("Vertical")]
+        Vertical = 1,
+    }
+}
