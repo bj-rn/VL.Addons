83f8e1872557a9cc5edb71e65470db91a8f11a29
cloneproduction <clone@cloneproduction.net>
2023-05-06 22:32:24 +0800

    Added Lomograph with Help Patch

---
+++ src/TextureFX/LomographType.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum LomographType
+    {
+        [Description("One")]
+        One = 0,
+        [Description("Two")]
+        Two = 1,
+        [Description("Three")]
+        Three = 2,
+        [Description("Four")]
+        Four = 3,
+        [Description("Five")]
+        Five = 4,
+        [Description("Six")]
+        Six = 5,
+        [Description("Gray")]
+        Gray = 6,
+        [Description("Sepia")]
+        Sepia = 7,
+    }
+}
