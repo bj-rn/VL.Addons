e2e4d7d1814750c064400cc2f3ce283323f44dc0
cloneproduction <clone@cloneproduction.net>
2023-05-06 20:13:07 +0800

    Added Quantize with Help Patch and Enums

---
+++ src/TextureFX/RoundingTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum RoundingType
+    {
+        [Description("Round")]
+        Round = 0,
+        [Description("Floor")]
+        Floor = 1,
+        [Description("Ceil")]
+        Ceil = 2,
+    }
+}
