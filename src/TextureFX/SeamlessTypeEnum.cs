d14f4fa82c199c899b2c566f3e03d99ec28f29e6
cloneproduction <clone@cloneproduction.net>
2023-05-08 22:03:35 +0800

    Added Seamless with Help Patch

---
+++ src/TextureFX/SeamlessTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.Common
+{
+    [Flags]
+    public enum SeamlessType
+    {
+        [Description("Technique1")]
+        Technique1 = 0,
+        [Description("Technique2")]
+        Technique2 = 1,
+        [Description("Technique3")]
+        Technique3 = 2,
+    }
+}
