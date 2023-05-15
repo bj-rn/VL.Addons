4d083da7f12757f05172c8133dd3e064ed347ffa
bj-rn <schwarzer@posteo.net>
2023-05-15 17:53:18 +0200

    Updates Dither

---
+++ src/TextureFX/DItherEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.TextureFX
+{
+    [Flags]
+    public enum Dither
+    {
+        [Description("Bayer")]
+        Bayer = 0,
+        [Description("PSX")]
+        PSX = 1,
+    }
+}
