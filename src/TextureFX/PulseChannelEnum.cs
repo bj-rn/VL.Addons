7372ba0a9a2f3363eb18ce23e93b861b42f13442
bj-rn <schwarzer@posteo.net>
2023-05-19 17:18:19 +0200

    Updates Pulse. Adds PulseChannelEnum

---
+++ src/TextureFX/PulseChannelEnum.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.TextureFX
+{
+    [Flags]
+    public enum PulseChannel
+    {
+        [Description("Red")]
+        Red = 0,
+        [Description("Green")]
+        Green = 1,
+        [Description("Blue")]
+        Blue = 2,
+        [Description("RGBtoHSV.x")]
+        RGBtoHSVx = 3,
+        [Description("RGBtoHSV.y")]
+        RGBtoHSVy = 4, 
+        [Description("RGBtoHSV.z")]
+        RGBtoHSVz = 5,
+        [Description("RGBtoHSL.z")]
+        RGBtoHSLz = 6,
+        [Description("Alpha")]
+        Alpha = 7
+    }
+}
