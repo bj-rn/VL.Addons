635d8573d89d897ee97db04762efcab17e2b78cf
cloneproduction <clone@cloneproduction.net>
2023-05-27 13:11:15 +0800

    Added ColorPalette with Help Patch

---
+++ src/TextureFX/PaletteTypeEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.TextureFX
+{
+    [Flags]
+    public enum PaletteType
+    {
+        [Description("HSL")]
+        HSL = 0,
+        [Description("HSV")]
+        HSV = 1,
+        [Description("Radial")]
+        Radial = 2,
+        [Description("Oklab")]
+        Oklab = 3,
+        [Description("CMYK")]
+        CMYK = 4,
+        [Description("YUV")]
+        YUV = 5,
+    }
+}
