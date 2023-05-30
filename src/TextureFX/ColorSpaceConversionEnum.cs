7066ec09b931162ca9d3c83ecff00bd0b81f08cf
bj-rn <schwarzer@posteo.net>
2023-05-30 14:30:21 +0200

    Adds ConvertColorSpace which combines GammaToLinear, SRBGToLinear. SRBGToLinearPrecise,LinearToGamma, LinearToSRGB, LinearToSRGBPrecise

---
+++ src/TextureFX/ColorSpaceConversionEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.TextureFX
+{
+    [Flags]
+    public enum ColorspaceConversion
+    {
+        [Description("Gamma to Linear")]
+        GammaToLinear = 0,
+        [Description("SRGB to Linear")]
+        SRGBToLinear = 1,
+        [Description("SRGB to Linear Precise")]
+        SRGBToLinearPrecise = 2,
+        [Description("Linear to Gamma")]
+        LinearToGamma = 3,
+        [Description("Linear to SRGB")]
+        LinearToSRGB = 4,
+        [Description("Linear to SRGB Precise")]
+        LinearToSRGBPrecise = 5
+    }
+}
