e2e2804b72ad75ecf3585285132330bef35fea9f
bj-rn <schwarzer@posteo.net>
2023-06-07 23:11:31 +0200

    Adds Halftone.

---
+++ src/TextureFX/HalftoneModeEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.TextureFX
+{
+    [Flags]
+    public enum HalftoneMode
+    {
+        [Description("Pixels")]
+        Pixels = 0,
+        [Description("Halftone")]
+        Halftone = 1,
+        [Description("SoftHalftone")]
+        SoftHalftone = 2,
+        [Description("BWHalftone")]
+        BWHalftone = 3,
+        [Description("LineHalftone")]
+        LineHalftone = 4,
+        [Description("TexturedHalftone")]
+        TexturedHalftone = 5
+    }
+}
