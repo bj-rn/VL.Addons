cc00bdd14a716a0cefc4e97195682c666ec8f8b2
cloneproduction <clone@cloneproduction.net>
2023-06-16 19:08:58 +0800

    Added all ToneMappings with Help Patch

---
+++ src/TextureFX/ToneMappingModeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.TextureFX
+{
+    [Flags]
+    public enum ToneMappingMode
+    {
+        [Description("Exposure")]
+        Exposure = 0,
+        [Description("KeyValue")]
+        KeyValue = 1,
+        [Description("AverageLuminance")]
+        AverageLuminance = 2,
+    }
+}
