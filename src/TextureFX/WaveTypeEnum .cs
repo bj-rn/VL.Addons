8a446c39b4cdf13cec422b82ccd16fe4b359b2fb
cloneproduction <clone@cloneproduction.net>
2023-06-28 09:12:04 +0800

    Addded Warp (Warp) with Help Patch

---
+++ src/TextureFX/WaveTypeEnum .cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.TextureFX
+{
+    [Flags]
+    public enum WaveType
+    {
+        [Description("Sine")]
+        Sine = 0,
+        [Description("Square")]
+        Square = 1,
+        [Description("Sawtooth")]
+        Sawtooth = 2,
+        [Description("Triangle")]
+        Triangle = 3,
+        [Description("Noise")]
+        Noise = 4,
+    }
+}
