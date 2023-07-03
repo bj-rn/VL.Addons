fd1a1811e52543709093a9cd52c6c04c0487fe2d
cloneproduction <clone@cloneproduction.net>
2023-07-03 16:50:29 +0800

    Added SetAlpha with Help Patch

---
+++ src/TextureFX/AlphaChannelEnum.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+
+namespace VL.Stride.Effects.TextureFX
+{
+    [Flags]
+    public enum AlphaChannel
+    {
+        [Description("Red")]
+        Red = 0,
+        [Description("Green")]
+        Green = 1,
+        [Description("Blue")]
+        Blue = 2,
+        [Description("Alpha")]
+        Alpha = 3,
+        [Description("Value")]
+        Value = 4,
+        [Description("Lightness")]
+        Lightness = 5,
+        [Description("Saturation")]
+        Saturation = 6,
+        [Description("Hue")]
+        Hue = 7,
+        [Description("Cyan")]
+        Cyan = 8,
+        [Description("Magenta")]
+        Magenta = 9,
+        [Description("Yellow")]
+        Yellow = 10,
+        [Description("Black")]
+        Black = 11,
+        [Description("AverageRGBA")]
+        AverageRGBA = 12,
+    }
+}
