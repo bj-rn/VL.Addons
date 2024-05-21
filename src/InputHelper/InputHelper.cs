da7f61cb16eb8a4b87e1eafaecfc893c158eeeb0
bj-rn <schwarzer@posteo.net>
2024-05-21 18:10:11 +0200

    add InputHelper to get GameControllerDevices form IInputSource

---
+++ src/InputHelper/InputHelper.cs
@@ -0,0 +1,32 @@
+﻿using Stride.Input;
+using VL.Lib.Collections;
+
+namespace VL.Stride.Input
+{
+    public static class InputHelper
+    {
+
+        public static IInputSource GetControllers(this IInputSource inputSource, out Spread<IGameControllerDevice> gameControllerDevices)
+        {
+
+            var controllers = new SpreadBuilder<IGameControllerDevice>();
+
+
+            if (inputSource != null)
+            {
+                foreach (var item in inputSource.Devices)
+                {
+                    var device = item.Value;
+
+                    if (device is IGameControllerDevice controller)
+                        controllers.Add(controller);
+                }  
+            }
+
+            gameControllerDevices = controllers.ToSpread();
+
+            return inputSource;
+        }
+
+    }
+}
